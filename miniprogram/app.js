90d6da2d8a278d950cf34c9fe330700d3abf6a84
lzh250586@alibaba-inc.com <lzh250586@alibaba-inc.com>
2020-09-02 13:44:07 +0800

    feat: init
    feat: init

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,5 +2,18 @@
 App({
   onLaunch: function () {
     this.globalData = {}
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'userInfo',
+      // 传给云函数的参数
+      data: {
+        a: 1,
+        b: 2,
+      },
+      success: function(res) {
+        console.log(res) // 3
+      },
+      fail: console.error
+    })
   }
 })

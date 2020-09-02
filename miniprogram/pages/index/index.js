b6e21acb3394a4206e2d0bf1b37a180e719fb060
unknown <unknown@example.com>
2020-09-02 09:17:53 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,36 @@
+// miniprogram/pages/index/index.js
+Page({
+  data: {
+    bannerList: [
+      'https://m0.autoimg.cn/cardfs/upload/spec/11852/800x0_q87_autohomecar__y_20120127095328825264.jpg',
+      'https://m0.autoimg.cn/cardfs/upload/spec/11852/800x0_q87_autohomecar__y_20120127095316018264.jpg',
+      'https://m0.autoimg.cn/cardfs/upload/spec/10003/800x0_q87_autohomecar__y_20111119102540861264.jpg',
+    ],
+    recommendList: [
+      {
+        id: 1,
+        imgUrl: 'https://m0.autoimg.cn/cardfs/upload/spec/10003/800x0_q87_autohomecar__y_20111119102105864264.jpg',
+        desc: 'ViViTiger珍珠亮银色定制版',
+        mark: '备注1' 
+      },
+      {
+        id: 2,
+        imgUrl: 'https://m0.autoimg.cn/cardfs/upload/spec/10003/800x0_q87_autohomecar__y_20111119102105864264.jpg',
+        desc: 'ViViTiger珍珠亮银色',
+        mark: '备注1' 
+      },
+      {
+        id: 3,
+        imgUrl: 'https://m0.autoimg.cn/cardfs/upload/spec/10003/800x0_q87_autohomecar__y_20111119102105864264.jpg',
+        desc: 'ViViTiger珍珠亮银色',
+        mark: '备注1' 
+      },
+      {
+        id: 4,
+        imgUrl: 'https://m0.autoimg.cn/cardfs/upload/spec/10003/800x0_q87_autohomecar__y_20111119102105864264.jpg',
+        desc: 'ViViTiger珍珠亮银色',
+        mark: '备注1' 
+      }
+    ]
+  },
+})

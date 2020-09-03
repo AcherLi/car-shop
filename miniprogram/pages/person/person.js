1e0c8b5324a57b1cd41e42fdc2ee25fc5d7a0cef
lzh <lzh>
2020-09-04 00:02:18 +0800

    feat: dev person center

---
+++ miniprogram/pages/person/person.js
@@ -0,0 +1,39 @@
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo: undefined
+  },
+  onLoad () {
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              this.setData({userInfo: res.userInfo})
+            }
+          })
+        } else {
+          this.setData({userInfo: null})
+        }
+      }
+    })
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          console.log(res)
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+  bindGetUserInfo (e) {
+    this.setData({userInfo: e.detail.userInfo})
+  },
+  getPhoneNumber (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+  }
+})
